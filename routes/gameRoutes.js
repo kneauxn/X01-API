1285eae2a84ba2f9ffc8b59e2b7223ee0700bc0f
kneauxn <kneauxn@gmail.com>
2016-12-22 03:15:36 -0700

    Don't know what this was ha.

---
+++ routes/gameRoutes.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+var Game = require('./models/gameModel');
+
+var routes = function(){
+var gameRouter = express.Router();
+ 
+ // Game history CRUD
+gameRouter.route('/Games')
+    .post(function(req, res){
+            var game = new Game(req.body);
+            game.save();
+            console.log(game);
+            res.status(201).send(game);
+
+        })
+    .get(function(req, res){
+        Game.find(function(err, games){
+            if(err){
+                res.status(500).send(err);
+            } else {
+                res.json(games);
+            }
+        });
+
+});
+
+app.use('/api', gameRouter);
+}
+
+module.exports = routes;
